aa72513bada049fb882b7ea0e667ca89623a959d
t1geryan <sgta1980@gmail.com>
2022-09-27 14:19:12 +0300

    made comments, change android manifest(change label)

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -14,12 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // scale animation (look at res/anim/scale.xml)
         val scaleAnim = AnimationUtils.loadAnimation(this,R.anim.scale)
 
-        //Input and Output Field
+        //Input and Output Fields
         val input = findViewById<TextView>(R.id.input)
         val output = findViewById<TextView>(R.id.output)
 
+
+        /// add text to math operation input
         fun setTextFields(str: String) {
             if (output.text.isNotEmpty()) {
                 input.text = output.text
@@ -28,60 +31,7 @@ class MainActivity : AppCompatActivity() {
             input.append(str)
         }
 
-        // Action Buttons List
-        val btnAC = findViewById<TextView>(R.id.btn_AC)
-        btnAC.setOnClickListener {
-            btnAC.startAnimation(scaleAnim)
-            input.text = ""
-            output.text = ""
-        }
-        val btnOB = findViewById<TextView>(R.id.btn_oBracket)
-        btnOB.setOnClickListener {
-            btnOB.startAnimation(scaleAnim)
-            setTextFields(btnOB.text.toString())
-        }
-        val btnCB = findViewById<TextView>(R.id.btn_cBracket)
-        btnCB.setOnClickListener {
-            btnCB.startAnimation(scaleAnim)
-            setTextFields(btnCB.text.toString())
-        }
-        val btnDiv = findViewById<TextView>(R.id.btn_div)
-        btnDiv.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnDiv.text.toString())
-        }
-        val btnProd = findViewById<TextView>(R.id.btn_prod)
-        btnProd.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnProd.text.toString())
-        }
-        val btnSum = findViewById<TextView>(R.id.btn_plus)
-        btnSum.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnSum.text.toString())
-        }
-        val btnSub = findViewById<TextView>(R.id.btn_sub)
-        btnSub.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnSub.text.toString())
-        }
-        val btnRes = findViewById<TextView>(R.id.btn_res)
-        btnRes.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            try {
-                val ex = ExpressionBuilder(input.text.toString()).build()
-                val result = ex.evaluate()
-
-                //  Вывод целых чисел без точки, а дробных с точкой
-                val longRes = result.toLong()
-                if (result == longRes.toDouble())
-                    output.text = longRes.toString()
-                else
-                    output.text = result.toString()
-            } catch (e:Exception) {
-                Log.d("Ошибка!", "сообщение ${e.message}")
-            }
-        }
+        // ALL BUTTONS EXCEPT AC, Back and Result (equal) add their text to the input field
 
         //Symbol Buttons List
         val btn0 = findViewById<TextView>(R.id.btn_0)
@@ -139,14 +89,75 @@ class MainActivity : AppCompatActivity() {
             it.startAnimation(scaleAnim)
             setTextFields(btnDot.text.toString())
         }
+        val btnOB = findViewById<TextView>(R.id.btn_oBracket)
+        btnOB.setOnClickListener {
+            btnOB.startAnimation(scaleAnim)
+            setTextFields(btnOB.text.toString())
+        }
+        val btnCB = findViewById<TextView>(R.id.btn_cBracket)
+        btnCB.setOnClickListener {
+            btnCB.startAnimation(scaleAnim)
+            setTextFields(btnCB.text.toString())
+        }
+        val btnDiv = findViewById<TextView>(R.id.btn_div)
+        btnDiv.setOnClickListener {
+            it.startAnimation(scaleAnim)
+            setTextFields(btnDiv.text.toString())
+        }
+        val btnProd = findViewById<TextView>(R.id.btn_prod)
+        btnProd.setOnClickListener {
+            it.startAnimation(scaleAnim)
+            setTextFields(btnProd.text.toString())
+        }
+        val btnSum = findViewById<TextView>(R.id.btn_plus)
+        btnSum.setOnClickListener {
+            it.startAnimation(scaleAnim)
+            setTextFields(btnSum.text.toString())
+        }
+        val btnSub = findViewById<TextView>(R.id.btn_sub)
+        btnSub.setOnClickListener {
+            it.startAnimation(scaleAnim)
+            setTextFields(btnSub.text.toString())
+        }
+
+        // Action Buttons List
+        val btnAC = findViewById<TextView>(R.id.btn_AC)
+        btnAC.setOnClickListener { // delete all io fields text
+            btnAC.startAnimation(scaleAnim)
+            input.text = ""
+            output.text = ""
+        }
         val btnBack = findViewById<TextView>(R.id.btn_back)
-        btnBack.setOnClickListener {
+        btnBack.setOnClickListener { // delete last symbol from input field and delete output field text at all
             it.startAnimation(scaleAnim)
             val str = input.text.toString()
             if (str.isNotEmpty())
                 input.text = str.substring(0, str.length-1)
             output.text = ""
         }
+
+        val btnRes = findViewById<TextView>(R.id.btn_res)
+        btnRes.setOnClickListener {
+            it.startAnimation(scaleAnim)
+            try {
+                // using library exp4j (take string get result)
+                val ex = ExpressionBuilder(input.text.toString()).build()
+                val result = ex.evaluate()
+
+                //Output of integers without a dot, and double numbers with a dot
+                val longRes = result.toLong()
+                if (result == longRes.toDouble())
+                    output.text = longRes.toString()
+                else
+                    output.text = result.toString()
+
+            } catch (e:Exception) { // if bad input
+                Log.d("Error!", "message: ${e.message}")
+            }
+        }
+
+
+
 
 
     }

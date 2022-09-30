98950272cc7c79e63ea80b3c89e72196eb128aeb
t1geryan <sgta1980@gmail.com>
2022-09-30 21:31:42 +0300

    lock activity main orientation at portrait in manifest

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         // scale animation (look at res/anim/scale.xml)
         val scaleAnim = AnimationUtils.loadAnimation(this,R.anim.scale)

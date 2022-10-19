3c94e50dc353982bad7e199615b4e7f7b81c3fa8
t1geryan <sgta1980@gmail.com>
2022-10-19 20:14:05 +0300

    change logic, make code prettier

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,166 +1,201 @@
 package com.example.calculator
 
-import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.animation.AnimationUtils
-import android.widget.TextView
+import com.example.calculator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.lang.Exception
+import kotlin.math.roundToLong
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // scale animation (look at res/anim/scale.xml)
-        val scaleAnim = AnimationUtils.loadAnimation(this,R.anim.scale)
-
-        //Input and Output Fields
-        val input = findViewById<TextView>(R.id.input)
-        val output = findViewById<TextView>(R.id.output)
-
-
-        /// add text to math operation input
-        fun setTextFields(str: String) {
-            if (output.text.isNotEmpty()) {
-                input.text = output.text
-                output.text = ""
-            }
-            input.append(str)
-        }
-
-        // ALL BUTTONS EXCEPT AC, Back and Result (equal) add their text to the input field
+        val scaleAnim = AnimationUtils.loadAnimation(this, R.anim.scale)
 
         //Symbol Buttons List
-        val btn0 = findViewById<TextView>(R.id.btn_0)
-        btn0.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn0.text.toString())
+        with(binding.btn0) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn1 = findViewById<TextView>(R.id.btn_1)
-        btn1.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn1.text.toString())
+        with(binding.btn1) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn2 = findViewById<TextView>(R.id.btn_2)
-        btn2.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn2.text.toString())
+        with(binding.btn2) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn3 = findViewById<TextView>(R.id.btn_3)
-        btn3.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn3.text.toString())
+        with(binding.btn3) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn4 = findViewById<TextView>(R.id.btn_4)
-        btn4.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn4.text.toString())
+        with(binding.btn4) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn5 = findViewById<TextView>(R.id.btn_5)
-        btn5.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn5.text.toString())
+        with(binding.btn5) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn6 = findViewById<TextView>(R.id.btn_6)
-        btn6.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn6.text.toString())
+        with(binding.btn6) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn7 = findViewById<TextView>(R.id.btn_7)
-        btn7.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn7.text.toString())
+        with(binding.btn7) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn8 = findViewById<TextView>(R.id.btn_8)
-        btn8.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn8.text.toString())
+        with(binding.btn8) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btn9 = findViewById<TextView>(R.id.btn_9)
-        btn9.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btn9.text.toString())
+        with(binding.btn9) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDigitText(this.text.toString())
+            }
         }
-        val btnDot = findViewById<TextView>(R.id.btn_dot)
-        btnDot.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnDot.text.toString())
+        with(binding.btnDot) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setDot(this.text.toString())
+            }
         }
-        val btnOB = findViewById<TextView>(R.id.btn_oBracket)
-        btnOB.setOnClickListener {
-            btnOB.startAnimation(scaleAnim)
-            setTextFields(btnOB.text.toString())
+        with(binding.btnOBracket) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setTextFields(this.text.toString())
+            }
         }
-        val btnCB = findViewById<TextView>(R.id.btn_cBracket)
-        btnCB.setOnClickListener {
-            btnCB.startAnimation(scaleAnim)
-            setTextFields(btnCB.text.toString())
+        with(binding.btnCBracket) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setTextFields(this.text.toString())
+            }
         }
-        val btnDiv = findViewById<TextView>(R.id.btn_div)
-        btnDiv.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnDiv.text.toString())
+        with(binding.btnDiv) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setSymbolText(this.text.toString())
+            }
         }
-        val btnProd = findViewById<TextView>(R.id.btn_prod)
-        btnProd.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnProd.text.toString())
+        with(binding.btnProd) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setSymbolText(this.text.toString())
+            }
         }
-        val btnSum = findViewById<TextView>(R.id.btn_plus)
-        btnSum.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnSum.text.toString())
+        with(binding.btnPlus) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setSymbolText(this.text.toString())
+            }
         }
-        val btnSub = findViewById<TextView>(R.id.btn_sub)
-        btnSub.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            setTextFields(btnSub.text.toString())
+        with(binding.btnSub) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                setSymbolText(this.text.toString())
+            }
         }
 
         // Action Buttons List
-        val btnAC = findViewById<TextView>(R.id.btn_AC)
-        btnAC.setOnClickListener { // delete all io fields text
-            btnAC.startAnimation(scaleAnim)
-            input.text = ""
-            output.text = ""
+        with(binding.btnAC) {
+            setOnClickListener { // delete all io fields text
+                startAnimation(scaleAnim)
+                binding.input.text = ""
+                binding.output.text = ""
+            }
         }
-        val btnBack = findViewById<TextView>(R.id.btn_back)
-        btnBack.setOnClickListener { // delete last symbol from input field and delete output field text at all
-            it.startAnimation(scaleAnim)
-            val str = input.text.toString()
-            if (str.isNotEmpty())
-                input.text = str.substring(0, str.length-1)
-            output.text = ""
-        }
-
-        val btnRes = findViewById<TextView>(R.id.btn_res)
-        btnRes.setOnClickListener {
-            it.startAnimation(scaleAnim)
-            try {
-                // using library exp4j (take string get result)
-                val ex = ExpressionBuilder(input.text.toString()).build()
-                val result = ex.evaluate()
-
-                //Output of integers without a dot, and double numbers with a dot
-                val longRes = result.toLong()
-                if (result == longRes.toDouble())
-                    output.text = longRes.toString()
-                else
-                    output.text = result.toString()
-
-            } catch (e:Exception) { // if bad input
-                Log.d("Error!", "message: ${e.message}")
+        with(binding.btnBack) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                back()
             }
         }
 
-
-
-
-
+        with(binding.btnRes) {
+            setOnClickListener {
+                startAnimation(scaleAnim)
+                calculate()
+            }
+        }
     }
+
+    private fun setTextFields(str: String) {
+        binding.input.append(str)
+    }
+
+    private fun setSymbolText(str: String) {
+        val input = binding.input.text.toString()
+        if ((input.isEmpty()) or ((input.isNotEmpty()) && (input.last() !in "+-/*")))
+            setTextFields(str)
+        else if ((input.isNotEmpty()) && (input.last() in "+-/*")) {
+            binding.input.text = input.substring(0, input.length-1)
+            setTextFields(str)
+        }
+    }
+    private fun setDigitText(str: String) {
+        setTextFields(str)
+        calculate()
+    }
+
+    private fun setDot(str: String) {
+        val lastInputNumber = binding.input.text.split('+', '-', '/', '*', '(', ')').last()
+        if ('.' !in lastInputNumber && binding.input.text.last() !in "+-/*()")
+            setTextFields(str);
+    }
+
+    private fun back() { // delete last symbol from input field and delete output field text at all
+        val str = binding.input.text.toString()
+        if (str.isNotEmpty())
+            binding.input.text = str.substring(0, str.length - 1)
+        calculate()
+    }
+    private fun calculate() { //  take result from input, calculate and input it in output
+        try {
+            // using library exp4j (take string get result)
+            val ex = ExpressionBuilder(binding.input.text.toString()).build()
+
+            val fiveDigitPrecision = 100_000.0;
+            val result = (ex.evaluate() * fiveDigitPrecision).roundToLong() / fiveDigitPrecision
+
+            //Output of integers without a dot, and double numbers with a dot
+            val longRes = result.toLong()
+            if (result == longRes.toDouble())
+                binding.output.text = longRes.toString()
+            else
+                binding.output.text = result.toString()
+
+        } catch (e: Exception) { // if bad input
+            Log.d("Error!", "message: ${e.message}")
+        }
+    }
+
 }
 
